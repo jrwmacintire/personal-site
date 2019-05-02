adaac713ad4aa76bb764e38af5a5094b2224b2a0
John MacIntire <jrwmacintire@gmail.com>
2019-05-02 11:22:03 -0700

    Added React structure. Added SCSS compiling.

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <h1>John MacIntire - Personal Website</h1>
+            </header>
+        )
+    }
+}
+
+export default Header;
