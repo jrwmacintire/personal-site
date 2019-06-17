885caa4435390e604e364bce55a3584d95f09047
John MacIntire <jrwmacintire@gmail.com>
2019-06-17 10:02:05 -0700

    Added Bootstrap and React Bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,12 @@ class Header extends Component {
     render() {
         return (
             <header className="Header">
-                <h1>John MacIntire - Personal Website</h1>
+                <h3 className="title">JohnMacIntire.com</h3>
+                <div className="nav-links">
+                    <a href="/contact">Contact</a>
+                    <a href="/services">Services</a>
+                    <a href="/about">About</a>
+                </div>
             </header>
         )
     }
