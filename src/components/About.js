f265cbb6be11a701ca9ab5a3a24236407e714060
John MacIntire <jrwmacintire@gmail.com>
2019-06-25 12:33:04 -0700

    Added Container, Row, and Col to About.

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import { Row, Col } from 'react-bootstrap';
 import Portrait from '../images/kiama-nsw-au.png';
 
 export default class About extends Component {
     render() {
         return (
-            <div className="About" >
-                <img src={ Portrait } alt='Kiama, New South Wales, Australia' />
-            </div>
+            <Container className="About" >
+                <Row>
+                    <Col md={4}>
+                        <img src={ Portrait } className='img-fluid' alt='Kiama, New South Wales, Australia' />
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
