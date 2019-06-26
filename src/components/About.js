532e786127d0707fc346ab0251b7289ea4b2007d
John MacIntire <jrwmacintire@gmail.com>
2019-06-26 09:54:39 -0700

    Adjusted React Bootstrap components to use native styling for centering

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 import { Row, Col } from 'react-bootstrap';
 import Portrait from '../images/kiama-nsw-au.png';
@@ -6,10 +7,19 @@ import Portrait from '../images/kiama-nsw-au.png';
 export default class About extends Component {
     render() {
         return (
-            <Container className="About" >
-                <Row>
-                    <Col md={4}>
-                        <img src={ Portrait } className='img-fluid' alt='Kiama, New South Wales, Australia' />
+            <Container className="About container-fluid" >
+                <Row className='about-bio-row justify-content-md-center'>
+                    <Col sm={10} md={8} lg={4} className='about-bio-col'>
+                        <div className='about-info-wrapper'>
+                            <p>Fullstack Javascript Developer and Designer based in Tucson, Arizona.</p>
+                            <p>I am an <b>Amazon Web Services (AWS) Certified Developer</b> who builds web apps in ReactJS and NodeJS on Serverless and Express server frameworks.</p>
+                            <Link to='/contact' className='about-contact-link'>Contact me</Link>
+                        </div>
+                    </Col>
+                    <Col sm={10} md={8} lg={4}>
+                        <div className='about-img-wrapper'>
+                            <img src={ Portrait } className='img-fluid' alt='Kiama, New South Wales, Australia' />
+                        </div>
                     </Col>
                 </Row>
             </Container>
