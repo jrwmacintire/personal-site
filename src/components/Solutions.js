79b738476d34d425bf8ea3158e157ab3825b1298
John MacIntire <jrwmacintire@gmail.com>
2019-07-06 09:57:32 -0700

    Adjusted text in Solutions

--- src/components/Solutions.js
+++ src/components/Solutions.js
@@ -14,11 +14,11 @@ export default class Solutions extends Component {
                         <h4>Mobile-ready Designs</h4>
                         <p>Your website's layout should adapt to your user's device. I ensure your products and info are available on all devices, even offline, so that you never miss a sale or subscription.</p>
                         <hr className='hr-divider'></hr>
-                        <h4>Focus on User Experience</h4>
-                        <p>Your website's layout must be clear and concise. Ensure anyone can access your products and/or services, allow me to provide you with a quality user experience.</p>
-                        <hr className='hr-divider'></hr>
                         <h4>Lower Costs - Migrate To Serverless</h4>
                         <p>Whether you would like to expand your site or establish an internet presence for a small business or project, serverless provides high performance and low cost.</p>
+                        <hr className='hr-divider'></hr>
+                        <h4>Focus on User Experience</h4>
+                        <p>Deliver your products and/or services with clear and concise webpages - contact me to discuss improving how users interact with your website.</p>
                         <div className='solutions-contact-link-wrapper'>
                             <Link to='/contact' className='solutions-contact contact-link light-gray-shadow'>Contact me</Link>
                         </div>
