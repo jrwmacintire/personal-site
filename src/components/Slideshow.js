d534a02bdfbdcd63548bd94d7433c1ebda37a875
John MacIntire <jrwmacintire@gmail.com>
2019-07-14 15:42:39 -0700

    Adding images to Carousel.

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -2,46 +2,53 @@ import React, { Component } from 'react'
 // import Container from 'react-bootstrap/Container';
 
 import Carousel from 'react-bootstrap/Carousel';
-// import Slide from './Slide';
 
 export default class Slideshow extends Component {
 
     constructor(props, context) {
         super(props, context);
 
-        this.getMockups = this.getMockups.bind(this);
         this.handleSelect = this.handleSelect.bind(this);
         this.state = {
             index: 0,
             direction: null,
-            mockups: []
+            mockupURLs: [
+                '../images/portfolio/DrumMachineMockup.png',
+                '../images/portfolio/LiteratuAusFullscreenMockup.png',
+                '../images/portfolio/MarkdownMockup.png',
+                '../images/portfolio/MicTVMockup.png',
+                '../images/portfolio/PersonalSiteMockup.png',
+                '../images/portfolio/PersonalSiteMockupMobile.png',
+                '../images/portfolio/SimonGameMockup.png',
+                '../images/portfolio/TheClosingCutMockup.png',
+                '../images/portfolio/TheClosingCutMockupMobile.png',
+            ],
+            url: '../images/portfolio/LiteratuAusFullscreenMockup.png'
         };
     }
 
-    componentDidMount() {
-        this.getMockups();
-    }
-
-    getMockups() {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            console.log(`event.target.value: ${e.target.value}`)
-        }
-    }
-
     handleSelect(selectedIndex, e) {
-
+        
     }
 
     render() {
-        const { index, direction } = this.state;
+        const { index, direction, url } = this.state;
+
         return (
             <div className="Slideshow">
                 <Carousel
                     activeIndex={index}
                     direction={direction}
                     onSelect={this.handleSelect} >
-                    { this.files }
+                    <Carousel.Item className='Slide'>
+                        <img
+                            className='d-block w-100'
+                            src={process.cwd() + url}
+                            alt={ `Slide ${index}`}></img>
+                        <Carousel.Caption>
+                            <h3>Slide {index}</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
                 </Carousel>
             </div>
         )
